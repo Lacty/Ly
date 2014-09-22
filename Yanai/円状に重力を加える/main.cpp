02faeecc47da1411396f4fd0fa079f32547bb44c
Lacty <akira206@gmail.com>
2014-09-22 18:44:08 +0900

    Earth

--- Yanai/円状に重力を加える/main.cpp
+++ Yanai/円状に重力を加える/main.cpp
@@ -4,8 +4,6 @@
 
 #include "appEnv.hpp"
 
-#define PI    3.14159265   // 円周率
-
 // アプリのウインドウサイズ
 enum Window {
   WIDTH  = 256,
@@ -13,25 +11,27 @@ enum Window {
 };
 
 struct Object{
-	Object(float x, float y, float radius, float division)
+	Object(float x, float y, float radius, float division,
+	float gr, float vy)
 	{
 		this->x = x;
 		this->y = y;
 		this->radius = radius;
 		this->division = division;
+		this->gr = gr;
+		this->vy = vy;
 	}
 	float x, y;
 	float radius;
 	float division;
+	float gr, vy;
 };
 
-Object earth(0, 0, 50, 300);
-Object player(0, 55, 5, 100);
+Object earth(0, 0, 50, 300, 0, 0);
+Object player(0, 55, 5, 100, 0.4, 0);
 
 float angle = 1.55;
 float ground = 110;
-float gr = 0.15;
-float vy = 0.0;
 void PlayerMove(AppEnv& app_env){
 	player.x = ground * cos(angle) / 2;
 	player.y = ground * sin(angle) / 2;
@@ -41,12 +41,12 @@ void PlayerMove(AppEnv& app_env){
 	if (app_env.isPressKey('A')){ angle += 0.03; }
 
 	// ジャンプさせる
-	if (app_env.isPushKey(GLFW_KEY_SPACE)){ vy = -3.5; }
+	if (app_env.isPushKey(GLFW_KEY_SPACE)){ player.vy = -5; }
 }
 
 void PlayerGravity(AppEnv& app_env){
-	ground -= vy;
-	vy += gr;
+	ground -= player.vy;
+	player.vy += player.gr;
 
 	if (ground <= 110){ ground = 110; }
 }
@@ -81,7 +81,6 @@ int main() {
 	PlayerGravity(app_env);
 
 	draw();
-
 
     // 
     // ここに描画処理を書く
