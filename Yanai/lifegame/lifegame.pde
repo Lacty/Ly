7c2ab44fd15e243c4a741c59ed0a7f27a3142ff3
Ly <akira206@gmail.com>
2014-12-01 11:09:52 +0900

    LifeGame作成中＠配列で詰み中

---
+++ Yanai/lifegame/lifegame.pde
@@ -0,0 +1,34 @@
+int windowSize = 400;
+int sideCells = 100;
+int cellSize = windowSize / sideCells;
+int FPS = 1;
+
+boolean[] bfield_x = new boolean[sideCells];
+boolean[] bfield_y = new boolean[sideCells];
+int[] ifield_x = new int[sideCells];
+int[] ifield_y = new int[sideCells];
+
+
+void setup(){
+  size(windowSize, windowSize);
+  frameRate(FPS);
+    for (int i = 0; i < sideCells; i++){
+    bfield_x[i] = true;
+    bfield_y[i] = true;
+  }
+  for(int i = 0; i < sideCells; i++){
+    ifield_x[i] = i*cellSize;
+    ifield_y[i] = i*cellSize;
+  }
+}
+
+void draw(){
+  background(100);
+  //println(ifield_x);
+  
+  for (int i = 0; i < sideCells; i++){
+    if ((bfield_x[i])&&(bfield_y[i])){
+      rect(ifield_x[i], ifield_y[i], cellSize, cellSize);
+    }
+  }
+}
---
+++ Yanai/円状に重力を加える/正常.cpp
@@ -0,0 +1,96 @@
+﻿//
+// アプリ雛形
+//
+
+#include "appEnv.hpp"
+
+// アプリのウインドウサイズ
+enum Window {
+  WIDTH  = 256,
+  HEIGHT = 256
+};
+
+struct Object{
+	Object(float x, float y, float radius, float division,
+	float gr, float vy)
+	{
+		this->x = x;
+		this->y = y;
+		this->radius = radius;
+		this->division = division;
+		this->gr = gr;
+		this->vy = vy;
+	}
+	float x, y;
+	float radius;      // 半径
+	float division;    // この値が大きいと円が滑らかになる
+	float gr, vy;
+};
+
+Object earth(0, 0, 50, 300, 0, 0);
+Object player(0, 55, 5, 100, 0.4, 0);
+
+float angle = 1.55;
+float ground = 110;
+
+void PlayerMove(AppEnv& app_env){
+	player.x = ground * cos(angle) / 2;
+	player.y = ground * sin(angle) / 2;
+	
+	// 角度を進める
+	if (app_env.isPressKey('D')){ angle -= 0.03; }
+	if (app_env.isPressKey('A')){ angle += 0.03; }
+
+	// ジャンプさせる
+	if (app_env.isPushKey(GLFW_KEY_SPACE)){ player.vy = -5; }
+}
+
+void PlayerGravity(AppEnv& app_env){
+	ground -= player.vy;
+	player.vy += player.gr;
+
+	if (ground <= 110){ ground = 110; }
+}
+
+void draw(){
+	drawFillCircle(earth.x, earth.y, earth.radius, earth.radius,
+		          earth.division, color256(200, 200, 200));
+
+	drawFillCircle(player.x, player.y, player.radius, player.radius,
+		player.division, Color(0, 0, 0));
+}
+
+// 
+// メインプログラム
+// 
+int main() {
+  // アプリウインドウの準備
+  AppEnv app_env(Window::WIDTH, Window::HEIGHT, false, false);
+  app_env.bgColor(Color(1, 1, 1));
+
+  // app_env.windowPosition(Vec2i(10, 10));
+
+  while (1) {
+    // アプリウインドウが閉じられたらプログラムを終了
+    if (!app_env.isOpen()) return 0;
+    
+    // 描画準備
+    app_env.setupDraw();
+
+	PlayerMove(app_env);
+
+	PlayerGravity(app_env);
+
+	draw();
+
+    // 
+    // ここに描画処理を書く
+    //
+
+    
+    // 画面更新
+    app_env.update();
+  }
+  
+  // アプリ終了
+}
